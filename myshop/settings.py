bc8115cf1c46a651334ebc670832aa1ef93b3973
Alexey Yrganov <lito66@mail.ru>
2023-04-11 23:39:18 +0400

    v1.6 cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'shop',
     'cart',
+    'orders',
 
 ]
 
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
